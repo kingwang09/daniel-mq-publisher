dda6bbb8304dc649953457f6e7c570bc1f8bb780
hyungeun <kingwang09@gmail.com>
2023-02-02 06:55:57 +0900

    초기 publisher 프로젝트 설정

---
+++ src/main/java/org/daniel/mq/publisher/DanielMqPublisherApplication.java
@@ -0,0 +1,13 @@
+package org.daniel.mq.publisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanielMqPublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanielMqPublisherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/daniel/mq/publisher/DanielMqPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package org.daniel.mq.publisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanielMqPublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
